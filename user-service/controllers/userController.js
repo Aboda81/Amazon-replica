c04dcf89b7025e5c28f0838c7746757e418eda2f
Abdelrahman Hussein <abdo.hussein812@gmail.com>
2024-06-27 06:35:43 +0300

    Intial Commit + User-service skeleton

---
+++ user-service/controllers/userController.js
@@ -0,0 +1,94 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { use } = require('../app');
+
+const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ where: { email } });
+        if (user) {
+            return res.status(400).json({ msg: 'User already exists' });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        const payload = {
+            user: {
+                id: user.id,
+            },
+        };
+
+        jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            }
+        );
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
+
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ where: { email } });
+        if (!user) {
+            return res.status(400).json({ msg: 'This email is not registered' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ msg: 'Wrong password' });
+        }
+
+        const payload = {
+            
+                id: user.id,
+        };
+
+        jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            }
+        );
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
+
+const getUserProfile = async (req, res) => {
+    try {
+        console.log(req.user)
+        const user = await User.findByPk(req.user.id, { attributes: { exclude: ['password'] } });
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getUserProfile,
+};
